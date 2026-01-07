844905c055f683d9e94c0013512fd5debbc4add5
Angela <a.barzaeva@gmail.com>
2026-01-07 02:34:03 +0100

    feat: add routing structure and app shell

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
+import { Karte } from './karte/karte';
+import { Formular } from './formular/formular';
+import { Success } from './success/success';
 
-export const routes: Routes = [{ path: 'mängel', component: App }];
+export const routes: Routes = [
+  { path: 'mängel', component: Karte },
+  { path: 'mängel/meldung', component: Formular },
+  { path: 'mängel/erfolg', component: Success },
+  { path: '', pathMatch: 'full', redirectTo: 'mängel' },
+  { path: '**', redirectTo: 'mängel' },
+];
